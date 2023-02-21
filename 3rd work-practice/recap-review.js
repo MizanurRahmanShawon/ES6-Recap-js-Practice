c4ab5f76a2277d7295bdf089eff33fabe34679bb
MizanurRahmanShawon <mr630961@gmail.com>
2023-02-21 22:48:30 +0600

    Recap-Review

---
+++ 3rd work-practice/recap-review.js
@@ -0,0 +1,27 @@
+/*
+ 1. Most know:
+ var, let, const,(important)
+ default parameter
+ 2. template string
+ create multi-line string(````)
+ add dynamic variable (${}')
+3. Arrow Function
+single parameter arrow function
+two parameter arrow function
+arrow function with function body 
+Map most Important,Find, Filter must*
+
+4. Good To Have
+1. Default parameter
+spread operation[...number, 12, 23]
+Destructuring/object destructuring
+array destructuring
+const[a,b,c]
+ForEach
+Class, object,Dot Notation vs Bracket notation.
+
+5..Should Know the Name
+Reduce,Inheritance,Prototypical Chain
+ 
+ 
+ */
